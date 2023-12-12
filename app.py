ff111d11c29c46946ee8d31736929d58ef0b3914
Renaras <152522370+Renaras@users.noreply.github.com>
2023-12-12 13:43:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ questoes={"Eu sou seu irmão, mas você não é meu irmão. Como isso é possív
           "Quantas patas tem um casal de patos?": "5",
           "Se o dia anterior a ontem é 21, que dia será que vem depois de amanhã?": "25",
           "O que sobe uma chaminé para baixo, mas não desce uma chaminé para cima?": "Um guarda-chuva",
-          "O que pode corree, mas nunca anda?": "O rio",
-          "Se eu entrei 4 vezes em um lugar, quantas vezes tenho que sair?": "3"}
+          "O que pode corree, mas nunca anda?": "O rio"}
 
 st.title("Jogo de Charadas")
 st.write("Adivinhe as respostas das charadas!")
