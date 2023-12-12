fc376f38a04a41375d333c1d31f36b4e6776eb10
Renaras <152522370+Renaras@users.noreply.github.com>
2023-12-12 13:27:27 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
+#Jogo de charadas
 import streamlit as st
-st.title('hello')
-num1=st.number_input('digite o primeiro número:')
-num2=st.number_input('digite o segundo número:')
 
-if st.button('soma'):
+questoes={"Eu sou seu irmão, mas você não é meu irmão. Como isso é possível?": "Você é minha irmã", 
+          "Quantas patas tem um casal de patos?": "5",
+          "Se o dia anterior a ontem é 21, que dia será que vem depois de amanhã?": "25",
+          "O que sobe uma chaminé para baixo, mas não desce uma chaminé para cima?": "Um guarda-chuva",
+          "O que pode corree, mas nunca anda?": "O rio",
+          "Se eu entrei 4 vezes em um lugar, quantas vezes tenho que sair?": "3"}
 
-    resultado=num1+num2
+st.title("Jogo de Charadas")
+st.write("Adivinhe as respostas das charadas!")
 
-    st.text('resultado:')
-    st.title('resultado')
+acertos = 0
+erros = 0
+
+for question, answer in questoes.items():
+    user_answer = st.text_input(question)
+    if user_answer.lower() == answer.lower():
+        acertos += 1
+        st.write("Resposta correta!")
+    else:
+        erros += 1
+        st.write("Resposta errada. A resposta correta é:", answer)
+
+st.write(f"Você acertou {acertos} charadas e errou {erros} charadas.")
